a1ef4db1e766205416b7ee50b2e4a2192b7fc1ec
ar.r.lysenko <ar.r.lysenko@tinkoff.ru>
2025-10-06 23:18:02 +0300

    add butching

--- src/test/java/org/itmo/BFSTest.java
+++ src/test/java/org/itmo/BFSTest.java
@@ -7,44 +7,11 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
 import org.itmo.bfs.impl.ForkJoinBfs;
 import org.itmo.bfs.impl.ParallelBfs;
+import org.itmo.bfs.impl.ParallelFrontierBfs;
 import org.itmo.bfs.impl.SimpleBfs;
 import org.junit.jupiter.api.Test;
 
 public class BFSTest {
-
-    @Test
-    public void kek() {
-        int size = 1_000_000;
-        int connections = 10_000_000;
-
-        Random r = new Random(42);
-
-        var g = new RandomGraphGenerator().generateGraph(r, size, connections);
-
-        long time1 = executeSerialBfsAndGetTime(g);
-        long time2 = executeForkJoinBfs(g);
-
-        System.out.println("Serial " + time1);
-        System.out.println("ForkJoin " + time2);
-    }
-
-    @Test
-    public void lotOfConnectionsTest() {
-        int size = 1_000_000;
-        int connections = 15_000_000;
-        Random r = new Random(42);
-
-        System.out.println("Generating graph of size " + size + " ...wait");
-        Graph g = new RandomGraphGenerator().generateGraph(r, size, connections);
-        System.out.println("Generation completed!\nStarting bfs");
-
-        long serialTime = executeSerialBfsAndGetTime(g);
-        long parallelTime = executeParallelBfsAndGetTime(g);
-
-        System.out.println("Times for " + size + " vertices and " + connections + " connections: ");
-        System.out.println("Serial: " + serialTime);
-        System.out.println("Parallel: " + parallelTime);
-    }
 
     @Test
     public void bfsTest() throws IOException {
@@ -59,10 +26,12 @@ public class BFSTest {
                 System.out.println("Generation completed!\nStarting bfs");
                 long serialTime = executeSerialBfsAndGetTime(g);
                 long forkJoinTime = executeForkJoinBfs(g);
+                long frontierTime = executeParallelFrontierBfsAndGetTime(g);
                 long parallelTime = executeParallelBfsAndGetTime(g);
                 fw.append("Times for " + sizes[i] + " vertices and " + connections[i] + " connections: ");
                 fw.append("\nSerial: " + serialTime);
                 fw.append("\nForkJoin: " + forkJoinTime);
+                fw.append("\nFrontier: " + frontierTime);
                 fw.append("\nParallel: " + parallelTime);
                 fw.append("\n--------\n");
             }
@@ -87,11 +56,20 @@ public class BFSTest {
         return (endTime - startTime) / 1_000_000;
     }
 
+    private long executeParallelFrontierBfsAndGetTime(Graph g) {
+        var runtime = Runtime.getRuntime().availableProcessors() * 2;
+        var bfs = new ParallelFrontierBfs(runtime);
+        long startTime = System.nanoTime();
+        bfs.bfs(g, 0);
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1_000_000;
+    }
+
     private long executeForkJoinBfs(Graph g) {
         var bfs = new ForkJoinBfs();
         long startTime = System.nanoTime();
-        try {
-            ForkJoinPool.commonPool().submit(() -> bfs.bfs(g, 0)).get();
+        try (var pool = ForkJoinPool.commonPool()) {
+            pool.submit(() -> bfs.bfs(g, 0)).get();
         } catch (InterruptedException | ExecutionException e) {
             throw new RuntimeException(e);
         }
